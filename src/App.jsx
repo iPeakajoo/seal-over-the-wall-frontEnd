280ff10703c376ceb63c140929f4169e9fc6b430
Khomgrit_Ton <khomgrit.ph@gmail.com>
2025-04-08 22:54:57 +0700

    style(file): created index.css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { Button } from "@/components/ui/button"
 import MUIButton from '@mui/material/Button';
+import './index.css'
+
 
 
 
@@ -13,10 +15,12 @@ export default function App() {
         <h1 className="w-full p-6 bg-amber-100 font-extrabold">
           React App Starter
         </h1>
-        <Button>Button from ShadCn</Button>
+        <Button className="bg-primary text-primary-foreground px-4 py-2 rounded-md">Button from ShadCn</Button>
         <MUIButton>Button from MUI</MUIButton>
         <MUIButton variant="contained">Contained</MUIButton>
-
+        <div class="bg-primary-blue-500 text-5xl font-bold">
+      Hello Tailwind
+        </div>
       </div>
     </div>
   );
